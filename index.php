2955446aa82c4a6cc21364831bd6aa959e7261f4
Andrea Gulli <andreagulli11@gmail.com>
2024-11-13 10:22:14 +0100

    add comments

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
 function getVocals(string $str): int {
 $count = 0;
 
+// Faccio un ciclo for per girare nella stringa e se trovo una vocale aggiungo al conteggo 1
 for ($i=0; $i < strlen($str) ; $i++) { 
     if (in_array($str[$i], ['a', 'e', 'i', 'o', 'u'])) {
         $count = $count + 1;
+    } else{
+        echo('Nella tua stringa non è presente alcuna vocale');
     }
 } 
+// Ritorno il numero di vocali che ho trovato nella stringa in caso ce ne fossero 
 return $count;
 }
 
